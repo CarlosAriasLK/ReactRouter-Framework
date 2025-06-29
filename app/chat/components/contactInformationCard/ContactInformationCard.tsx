71731ea7878d96f801570f34370d40f9d0840d94
Carlos Arias <ceau922@gmail.com>
2025-06-29 15:57:41 -0500

    contact info dependiendo del id que se pase

--- app/chat/components/contactInformationCard/ContactInformationCard.tsx
+++ app/chat/components/contactInformationCard/ContactInformationCard.tsx
@@ -1,13 +1,25 @@
-import { Button } from "~/components/ui/button"
-import { ContactInformation } from "./ContactInformation"
-import { ContactInformationSkeleton } from "./ContactInformationSkeleton"
-import { NoContactSelected } from "./NoContactSelected"
+import { useLoaderData, useNavigation, useParams } from "react-router";
+
+import { ContactInformation } from "./ContactInformation";
+import { ContactInformationSkeleton } from "./ContactInformationSkeleton";
+import { NoContactSelected } from "./NoContactSelected";
+import type { Client } from "~/chat/interfaces/chat.interfaces";
 
 
 export const ContactInformationCard = () => {
-    /* return <ContactInformationSkeleton />  */
-    
-    return <NoContactSelected /> 
 
-    /* return <ContactInformationSkeleton /> */
+    const { id } = useParams();
+    const { clients = [] } = useLoaderData();
+
+    const { state } = useNavigation();
+
+    const isPending = state === 'loading';
+    const client = clients.find( (client:Client) => client.id === id );
+
+    if ( isPending ) return <ContactInformationSkeleton />;
+    if ( !id ) return <NoContactSelected />;
+    if ( !client ) return <NoContactSelected />;
+    
+    return <ContactInformation client={ client } />;
+    
 }
